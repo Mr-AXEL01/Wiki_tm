f30702f64ae7e8286e840a02fd8d2a38d32991b2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-08 17:35:34 +0100

    in the function getUrl() we check if it exist using the isset function and if it's true we remove the last slash in the right using the function rtrim and after we remove any caracter a url should not have using FILTER_SANITIZE_URL inside a function filter_var

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -14,6 +14,14 @@ class Core {
     }
 
     public function getUrl() {
-       echo $_GET['url'];
+       // check if there is a url add 
+       if (isset($_GET['url'])) {
+        // remove any end-slashes if it's exists
+        $url = rtrim($_GET['url'], '/');
+        // remove any caracter a url should not have
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        // break the url into array of parts
+        $url = explode('/',$url);
+        return $url;
     }
 }
