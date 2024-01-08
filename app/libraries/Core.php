be036c4c41c3fbe9e9c734b76df274a04ccfe8ea
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-08 16:13:27 +0100

    create class core  inside the core file and set a propertie currentController so like that if the router didn't find the controller that given in the link it will take pages as a default controller

---
+++ app/libraries/Core.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* App Core Class
+* => Create URL & Load core controller
+* => URL Format - /controller/method.params
+*/
+class Core {
+    protected $currentController = 'Pages';
+    
+
+}
