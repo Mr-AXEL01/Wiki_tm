bfc7e9d327426e59c032711c3596efd7d55834cb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-08 16:21:03 +0100

    create a magic method contruct for the core class

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -8,4 +8,8 @@ class Core {
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
+
+    public function __construct() {
+        
+    }
 }
