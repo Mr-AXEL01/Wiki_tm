b8dc9466abde401a5ef09a0d40d0bf94dc010a69
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 20:11:54 +0100

    create a footer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,8 @@
-
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            &copy; 2024 Your Wiki Platform. All rights reserved.
+        </div>
+    </footer>
     <script src="<?php URLROOT;?>/js/main.js"></script>
 </body>
 </html>
