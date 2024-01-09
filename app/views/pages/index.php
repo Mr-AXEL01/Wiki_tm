282c17036c32f53092620005da35174b2e12222c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 17:25:33 +0100

    create navbar and set new logo

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php';?>
+<?php require APPROOT . '/views/inc/navbar.php';?>
 <h1><?= $data['title'];?></h1>
+<p><?php echo APPROOT ;?></p>
 <?php require APPROOT . '/views/inc/footer.php';?>
