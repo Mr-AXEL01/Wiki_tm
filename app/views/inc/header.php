3f6e1ab6776f046070364e6f241aba230c76ec25
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 03:24:42 +0100

    create file css in folder css in the public and create mainJs file in the Js folder . and create folder of includes in views folder to set header and footer files

---
+++ app/views/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=URLROOT;?>/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?= SITENAME;?></title>
+    
+</head>
+<body>
+    
+</body>
+</html>
