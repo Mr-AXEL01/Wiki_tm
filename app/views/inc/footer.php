b5c01f39ceab4f26fb011f1370e31cebaf6bdd1c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 21:58:39 +0100

    set up the landing pages

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,5 @@
-    <footer class="bg-gray-800 text-white py-4">
-        <div class="container mx-auto text-center">
+    <footer class="bg-gray-100 h-[6vh] text-white py-4">
+        <div class="container mx-auto text-gray-800 text-center">
             &copy; 2024 Your Wiki Platform. All rights reserved.
         </div>
     </footer>
