8c41841a712985d3202fe945ac4c138b720a7d10
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 03:03:49 +0100

    create autouad core for libraries to load the file that we need from the folder libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,7 @@
 // Load Config
 require_once 'config/config.php';
 
-// Load the libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+// Autoload Core Libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+});
