634752197c929a723772c0d26f227450dadf2c97
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 22:08:27 +0100

    create a users controller to handle the register

---
+++ app/controllers/Users.php
@@ -0,0 +1,32 @@
+<?php
+
+class Users {
+    public function __construct() {
+
+    }
+    public function register() {
+        // check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // process form
+        } else {
+            // load form
+            $data = [
+                'fullName' =>'',
+                'username' =>'',
+                'pictureUser' =>'',
+                'email' =>'',
+                'password' =>'',
+                'confirm_password' =>'',
+                'fullName_err' =>'',
+                'username_err' =>'',
+                'pictureUser_err' =>'',
+                'email_err' =>'',
+                'password_err' =>'',
+                'confirm_password_err' =>''
+            ];
+
+            // load view
+            $this->view('users/register', $data);
+        }
+    }
+}
