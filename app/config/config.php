7f2c3171cccc5f18e547d4c6d2a00bbfe2e33308
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-09 09:26:38 +0100

    create consts for our Database Params

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+// DB Paramas
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'wiki_tm');
+
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
