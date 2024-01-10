ef8f5a38a911e8d30b9586604b68972d6763b095
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-10 10:00:03 +0100

    create properties for the User model

---
+++ app/models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+class User {
+    private $idUser;
+    private $fullName; 
+    private $username; 
+    private $pictureUser; 
+    private $email; 
+    private $password; 
+    private $role;
+}
