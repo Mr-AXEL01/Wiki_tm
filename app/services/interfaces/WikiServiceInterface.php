594825c9065768080ac098c9b74bb9bfbd70d0bd
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-12 11:03:49 +0100

    update the interface for wiki

--- app/services/interfaces/WikiServiceInterface.php
+++ app/services/interfaces/WikiServiceInterface.php
@@ -2,9 +2,13 @@
 
 interface WikiServiceInterface {
     public function read();
+    public function readArchived();
+    public function wikisOfAuthor($idUser);
     public function create(Wiki $wiki);
-
+    public function update(Wiki $wiki);
+    public function delete($idWiki);
+    public function softDelete($idWiki);
+    public function restore($idWiki);
+    public function fetch($idWiki);
+    public function getColumns();
 }
-
-
-?>
