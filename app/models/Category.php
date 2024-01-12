7662972ba0bf0affedcb595cd96efcbe75ffee76
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-12 09:05:00 +0100

    create model for category

---
+++ app/models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+class Category {
+    private $idCategory;
+    private $nameCategory; 
+    private $description; 
+    private $pictureCategory;
+
+    public function __construct() {
+
+    }
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this->$property;
+    }
+}
