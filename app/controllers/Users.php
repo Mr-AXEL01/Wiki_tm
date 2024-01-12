6e236ee8f309dfb3c313a73cc3194bf667b3d951
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-12 01:35:52 +0100

    debuug register

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,95 +1,86 @@
 <?php
 
 class Users extends Controller {
-    private $model;
+    // private $model;
     private $service;
-    public function __construct() {
-        $this->model = $this->model("User");
+    public function __construct() 
+    {
+        // $this->model = $this->model("User");
         $this->service = $this->service("UserServiceImp");
     }
-    public function register() {
-        // check for POST
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // process form
 
-            // Sanitize Post data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            // Init data
-            $data = [
-                'pictureUser' => $_FILES['pictureUser']["tmp_name"],
-                'fullName' =>trim($_POST['fullName']),
-                'username' =>trim($_POST['username']),
-                'email' =>trim($_POST['email']),
-                'password' =>trim($_POST['password']),
-                'confirm_password' =>trim($_POST['confirm_password']),
-            ];
-
-            if(!empty($data['email']) && !empty($data['fullName']) && !empty($data['username']) && !empty($data['password'])) {
-
-                // hash password
-                $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
-                extract($data, EXTR_SKIP);
-                // Register User
-                $this->model->__set("pictureUser", $pictureUser);
-                $this->model->__set("fullName", $fullName);
-                $this->model->__set("username", $username);
-                $this->model->__set("email", $email);
-                $this->model->__set("password", $password);
-                $userService = $this->service->create($this->model);
+    public function login()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $loggedInUser = $this->service->login($email, $password);
+            // var_dump($this->service->isAuthor($loggedInUser->user_id));
+            if ($loggedInUser) {
+                $this->createSession($loggedInUser);
+                if ($this->service->isAuthor($loggedInUser->user_id)) {
+                    redirect('author/dashboard');
+                } else {
+                    redirect('admin/dashboard');
+                }
+            } else {
+                $this->view("users/login");
             }
 
-        } else {
-            // load form
-            $data = [
-                'pictureUser' =>'',
-                'fullName' =>'',
-                'username' =>'',
-                'email' =>'',
-                'password' =>'',
-                'confirm_password' =>''
-            ];
-
-            // load view
-            $this->view('users/register', $data);
         }
+
+        $this->view("users/login");
     }
 
-    public function login() {
-        
-        // check for POST
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // process form
-
-            // Sanitize Post data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-            // Init data
-            $data = [
-                'email' =>trim($_POST['email']),
-                'password' =>trim($_POST['password'])
-            ];
-
-
-            extract($data, EXTR_SKIP);
-            $this->model->__set("email", $email);
-            $this->model->__set("password", $password);
-            $result =  $this->service->fetchByEmail($this->model);
-            print_r($result);
-
-
-
-        } else {
-            // load form
-            $data = [
-                'email' =>'',
-                'password' =>''
-            ];
-
-            // load view
-            $this->view('users/login');
-            
+    public function register()
+    {
+        if($_SERVER["REQUEST_METHOD"] == "POST" ){
+            var_dump($_POST);
+            $fullName = $_POST["fullName"];
+            $username = $_POST["username"];
+            $pictureUser = $_POST["pictureUser"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $newUser = new User();
+            $newUser->fullName = $fullName;
+            $newUser->username = $username;
+            $newUser->pictureUser = $pictureUser;
+            $newUser->email = $email;
+            $newUser->password = $password;
+            try{
+                $this->service->create($newUser);
+                redirect('users/login');
+               }
+               catch(PDOException $e){
+                die($e->getMessage());
+               }
         }
+        $this->view("users/register");
+    }
 
+    public function createSession($user)
+    {
+        $_SESSION["idUser"] = $user->idUser;
+        $_SESSION["fullName"] = $user->fullName;
+        $_SESSION["email"] = $user->email;
+        // header("Location:".URLROOT."client/product");
+    }
+    
+    public function destroySession($user)
+    {
+        unset($_SESSION["idUser"]);
+        unset($_SESSION["fullName"]);
+        unset($_SESSION["email"]);
+        session_destroy();
+        redirect('users/login');
+    }
+    
+    public function islogged()
+    {
+        if(isset($_SESSION["idUser"])){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- app/services/implementations/UserServiceImp.php
+++ app/services/implementations/UserServiceImp.php
@@ -1,7 +1,6 @@
 <?php
 
-class UserServiceImp implements UserServiceInterface
-{
+class UserServiceImp implements UserServiceInterface{
     private $db;
     private $dbh;
 
@@ -13,73 +12,106 @@ class UserServiceImp implements UserServiceInterface
 
     public function create(User $user)
     {
+        $dbh = $this->dbh;
         if (empty($user->role)) {
             $user->role = 'author';
         }
-        
-        $stmt = $this->dbh->prepare('INSERT INTO user (fullName, username, pictureUser, email, password, role) 
-                          VALUES (:fullName, :username, :pictureUser, :email, :password, :role)');
-        $fullName = $user->__get("fullName");
-        $username = $user->__get("username");
-        $pictureUser = $user->__get("pictureUser");
-        $email = $user->__get("email");
-        $pass = $user->__get("password");
-        $role = $user->__get("role");
-        
-        $stmt->bindParam(':fullName',$fullName );
-        $stmt->bindParam(':username',$username );
-        $stmt->bindParam(':pictureUser',$pictureUser );
-        $stmt->bindParam(':email',$email );
-        $stmt->bindParam(':password',$pass );
-        $stmt->bindParam(':role',$role );
-        
-        
-        $stmt->execute();
+        $sql = "INSERT INTO user (fullName, username, pictureUser, email, password, role) 
+        VALUES (:fullName, :username, :pictureUser, :email, :password, :role)";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([
+            ":fullName" => $user->fullName,
+            ":username" => $user->username,
+            ":pictureUser" => $user->pictureUser,
+            ":email" => $user->email,
+            ":password" => password_hash($user->password, PASSWORD_DEFAULT),
+            ":role" => $user->role
+        ]);
+        $dbh = null;
+        $stmt = null;
     }
 
     public function read()
     {
+        $dbh = $this->dbh;
         $sql = "SELECT * FROM user ORDER BY idUser DESC";
-
-        $this->db->query($sql);
-        return $this->db->resultSet();
-    }
-
-    public function fetchByEmail(User $user){
-        $email = $user->__get("email");
-        $pass = $user->__get("password");
-        $stmt = $this->dbh->prepare('SELECT * FROM user WHERE email = :email');
-        $stmt->bindParam(':email',$email );
+        $stmt = $dbh->prepare($sql);
         $stmt->execute();
-        if($stmt->rowCount() > 0){
-            $userInfo = ($stmt->fetch());
-            if(password_verify($pass,$userInfo['password'])) {
-                if($userInfo['role'] == 'admin'){
-                    header('Location:..');
-                }else{
-                    header('Location:....');
-                }
-            }else {
-                print_r('password incorrect');
-            }
-        }else {
-            redirect('users/login');
+        $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $dbh =null;
+        $stmt = null;
+        return $result;
+    }
+
+    public function login($email, $password)
+{
+    $dbh = $this->dbh;
+    $query = "SELECT * FROM user WHERE email = :email";
+    $results = $dbh->prepare($query);
+    $results->execute([":email" => $email]);
+
+    if ($results) {
+        $stmt = $results->fetch(PDO::FETCH_OBJ);
+
+        if ($stmt && password_verify($password, $stmt->password)) {
+            return $stmt;
         }
+    }
+
+    return false;
+}
+
+
+    public function isAuthor($idUser)
+    {
+        $dbh=$this->dbh;
+        $query = "SELECT * FROM user WHERE idUser=:idUser AND role='author'";
+        $results = $dbh->prepare($query);
+        $results->execute([":idUser"=> $idUser]);
+        $results->fetch(PDO::FETCH_OBJ);
+
+        if($results->rowCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // public function fetchByEmail(User $user){
+    //     $email = $user->__get("email");
+    //     $pass = $user->__get("password");
+    //     $stmt = $this->dbh->prepare('SELECT * FROM user WHERE email = :email');
+    //     $stmt->bindParam(':email',$email );
+    //     $stmt->execute();
+    //     if($stmt->rowCount() > 0){
+    //         $userInfo = ($stmt->fetch());
+    //         if(password_verify($pass,$userInfo['password'])) {
+    //             if($userInfo['role'] == 'admin'){
+    //                 header('Location:..');
+    //             }else{
+    //                 header('Location:....');
+    //             }
+    //         }else {
+    //             print_r('password incorrect');
+    //         }
+    //     }else {
+    //         redirect('users/login');
+    //     }
         
-    }
+    // }
 
-    public function fetch($idUser)
-    {
-        $this->db->query('SELECT * FROM user WHERE idUser = :idUser');
-        $this->db->bind(':idUser', $idUser);
-        return $this->db->single();
-    }
+    // public function fetch($idUser)
+    // {
+    //     $this->db->query('SELECT * FROM user WHERE idUser = :idUser');
+    //     $this->db->bind(':idUser', $idUser);
+    //     return $this->db->single();
+    // }
 
-    private function bindValues(User $user)
-    {
-        foreach ($user as $property => $value) {
-            die(':' . $property. $value);
-            // $this->db->bind(':' . $property, $value);
-        }
-    }
+    // private function bindValues(User $user)
+    // {
+    //     foreach ($user as $property => $value) {
+    //         die(':' . $property. $value);
+    //         // $this->db->bind(':' . $property, $value);
+    //     }
+    // }
 }
